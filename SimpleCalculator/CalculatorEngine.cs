b95611fa82f1161f6c376cbf38053945040aab3f
ayoub129 <ayoub.berouijel10@gmail.com>
2023-10-04 14:47:20 +0200

    Add project files.

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate(string operation , double firstnum , double secondnum)
+        {
+            double result;
+
+            switch(operation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = firstnum + secondnum;
+                    break;
+                case "subtract":
+                case "-":
+                    result = firstnum - secondnum;
+                    break;
+                case "multiply":
+                case "*":
+                    result = firstnum * secondnum;
+                    break;
+                case "divide":
+                case "/":
+                    result = firstnum / secondnum;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation not recognized");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            InputConverter inputconverter = new InputConverter();
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+            double firstNumber = inputconverter.Convert(Console.ReadLine());
+            double secondNumber = inputconverter.Convert(Console.ReadLine());
+            string operation = Console.ReadLine();
+
+            double result = calculatorEngine.Calculate(operation , firstNumber , secondNumber );
+            Console.WriteLine( result );
+
+            } catch (Exception e)
+            {
+                // TODO: start loging this exception message
+                Console.WriteLine( e.Message );
+            }
+        }
+    }
+}
