c1ca148680cbddf955d90bb67093d877520e82d3
ayoub129 <ayoub.berouijel10@gmail.com>
2023-10-04 15:03:53 +0200

    Adding some unit test

---
+++ simpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+using System;
+
+namespace simpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            int number = 0;
+            int number2 = 2;
+            double number3 = _calculatorEngine.Calculate("add" , number , number2) ;  
+            
+            Assert.AreEqual(2, number3);
+        }
+
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number = 0;
+            int number2 = 2;
+            double number3 = _calculatorEngine.Calculate("+", number, number2);
+
+            Assert.AreEqual(2, number3);
+        }
+    }
+}
---
+++ simpleCalculator.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("simpleCalculator.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("simpleCalculator.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7aaa587-0f88-45a4-84cc-775fe07781d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
